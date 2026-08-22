b4789f1a2ca1ab7c632324576655e286328d7cda
uijin219 <uj4811@naver.com>
2026-08-22 22:55:43 +0900

    - 내비게이션 바 & 햄버거 메뉴 버튼 반응 구현
    - 페이징 기능 & 내림차순 정렬 구현

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.domain.Page;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,9 +25,9 @@ public class QuestionController {
 	private final QuestionService questionService;
 	
 	@GetMapping("/list")
-	public String list(Model model) {
-		List<Question> questionList = this.questionService.getList();
-		model.addAttribute("questionList", questionList);
+	public String list(Model model, @RequestParam(value="page", defaultValue="0")int page) {
+		Page<Question> paging = this.questionService.getList(page);
+		model.addAttribute("paging", paging);
 		return "question_list";
 	}
 	
--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,11 +1,18 @@
 package com.mysite.sbb.question;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.time.LocalDateTime;
 
 import com.mysite.sbb.DataNotFoundException;
+
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -13,6 +20,14 @@ import lombok.RequiredArgsConstructor;
 
 public class QuestionService {
 	private final QuestionRepository questionRepository;
+	
+	public Page<Question> getList(int page){
+		List<Sort.Order> sorts = new ArrayList<>();
+		sorts.add(Sort.Order.desc("createDate"));
+		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+		return this.questionRepository.findAll(pageable);
+	}
+	
 	public List<Question> getList(){
 		return this.questionRepository.findAll();
 	}
